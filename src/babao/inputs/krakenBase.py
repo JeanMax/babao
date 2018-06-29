7545aa22ee156449487ac751386f6478042fc2f7
JeanMax <mcanal@student.42.fr>
2018-06-29 23:02:10 +0200

    Tweak kraken

--- src/babao/inputs/krakenBase.py
+++ src/babao/inputs/krakenBase.py
@@ -5,6 +5,7 @@ TODO
 import time
 import http
 import socket
+import requests
 from abc import abstractmethod
 import krakenex
 
@@ -16,6 +17,19 @@ API = None
 API_DELAY = 3
 
 
+def _initAPI():
+    """TODO"""
+    global API
+    API = krakenex.API()
+    try:
+        API.load_key(conf.API_KEY_FILE)
+    except Exception as e:  # TODO
+        log.warning(
+            "Couldn't load kraken api key file '"
+            + conf.API_KEY_FILE + "': " + repr(e)
+        )
+
+
 class ABCKrakenInput(ABCInput):
     """
     Base class for any kraken input
@@ -24,36 +38,29 @@ class ABCKrakenInput(ABCInput):
     def __init__(self):
         super().__init__()
         self.__tick = None
-        global API
         if API is None:
-            API = krakenex.API()
-        if API.key == '':
-            try:
-                API.load_key(conf.API_KEY_FILE)
-            except Exception as e:  # TODO
-                log.warning(
-                    "Couldn't load kraken api key file '"
-                    + conf.API_KEY_FILE + "': " + repr(e)
-                )
+            _initAPI()
 
     def _doRequest(self, method, req=None):
         """General function for kraken api requests"""
 
-        self._sleep()
-        if not req:
+        if req is None:
             req = {}
+        if method == "Trades":
+            query = API.query_public
+        else:
+            query = API.query_private
 
         # we loop in case of request error (503...)
-        fail_counter = 1
-        while fail_counter > 0:  # really nice loop bro, respect... no goto tho
+        fail_counter = 0
+        while True:  # TODO: do you really want an infinite loop?
+            self._sleep()
             try:
-                if method == "Trades":
-                    res = API.query_public(method, req)
-                else:
-                    res = API.query_private(method, req)
+                res = query(method, req, timeout=42)
             except (
                     socket.timeout,
                     socket.error,
+                    requests.HTTPError,
                     http.client.BadStatusLine,
                     http.client.CannotSendRequest,
                     ValueError
@@ -68,9 +75,10 @@ class ABCKrakenInput(ABCInput):
                         log.warning("Exception returned by Kraken API!\n" + e)
                 else:
                     return res["result"]
-            log.debug("Connection fail #" + str(fail_counter))
             fail_counter += 1
-            self._sleep()
+            if not fail_counter % 10:
+                _initAPI()  # have you tried turning it off and on again?
+            log.debug("Connection fail #" + str(fail_counter))
 
         return None  # warning-trap
 
@@ -79,7 +87,7 @@ class ABCKrakenInput(ABCInput):
         if self.__tick is not None:
             delta = time.time() - self.__tick
             log.debug(
-                "Loop took " + str(round(delta, 3)) + "s ("
+                "Request took " + str(round(delta, 3)) + "s ("
                 + self.__class__.__name__ + ")"
             )
         else:
